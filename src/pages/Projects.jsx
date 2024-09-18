c3a9e5dddcaf5e1cf63e09933485d996a3ece49e
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-09-18 13:23:40 +0200

    styled and fixed all projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import { supabase } from "../utils/supabase";
 import { Link } from "react-router-dom";
 import CreateProject from "../components/CreateProject";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Accordion from "react-bootstrap/Accordion";
+import ProjectCard from "../components/ProjectCard";
 
 const Projects = () => {
   const [projects, setProjects] = useState(null);
@@ -41,37 +47,120 @@ const Projects = () => {
 
   return (
     <>
-      <div>
-        <h3>Create new project</h3>
-        <button onClick={handleOpenCreateProject}>Open Create Project</button>
-        {showCreateProject && (
-          <CreateProject onClose={handleCloseCreateProject} />
-        )}
-        <h1>All Projects</h1>
-        {projects ? (
-          projects.length > 0 ? (
-            <ul>
-              {projects.map((project) => (
-                <Link
-                  to={`/projects/${project.id}`}
-                  state={{ project }}
-                  key={project.id}
-                >
-                  <li>
-                    <strong>ID:</strong> {project.id}, <strong>Name:</strong>{" "}
-                    {project.name}, <strong>Created At:</strong>{" "}
-                    {new Date(project.created_at).toLocaleString()}
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          ) : (
-            <p>No data found in the Projects table.</p>
-          )
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
+      <Container>
+        <Container>
+          <Row>
+            <Col>
+              <h1>Alla Projekt</h1>
+            </Col>
+            <Col>
+              <button onClick={handleOpenCreateProject}>
+                Skapa Nytt Projekt
+              </button>
+              {showCreateProject && (
+                <CreateProject onClose={handleCloseCreateProject} />
+              )}
+            </Col>
+          </Row>
+        </Container>
+        <hr className="allproduct-border"></hr>
+        <Container>
+          <Row>
+            <Col className="col-lg-3 col-md-12">
+              <Col>
+                <h3>Filter</h3>
+              </Col>
+              <Accordion
+                defaultActiveKey={["0", "1", "2", "3", "4"]}
+                alwaysOpen
+                flush
+              >
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Fritext</Accordion.Header>
+                  <Accordion.Body>
+                    <Form>
+                      <Row>
+                        <Col>
+                          <Form.Control
+                            type="text"
+                            placeholder="Fritext"
+                            className=" mr-sm-2"
+                          />
+                        </Col>
+                      </Row>
+                    </Form>{" "}
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Land</Accordion.Header>
+                  <Accordion.Body>
+                    <Form.Select aria-label="Default select example">
+                      <option>Välj land</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </Form.Select>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                  <Accordion.Header>Region</Accordion.Header>
+                  <Accordion.Body>
+                    {" "}
+                    <Form.Select aria-label="Default select example">
+                      <option>Välj Region</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </Form.Select>{" "}
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                  <Accordion.Header>Organisation</Accordion.Header>
+                  <Accordion.Body>
+                    {" "}
+                    <Form.Select aria-label="Default select example">
+                      <option>Välj Organisation</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </Form.Select>{" "}
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="4">
+                  <Accordion.Header>Status</Accordion.Header>
+                  <Accordion.Body>
+                    {" "}
+                    <Form.Select aria-label="Default select example">
+                      <option>Aktivt</option>
+                      <option value="1">One</option>
+                    </Form.Select>{" "}
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Col>
+            <Col>
+              <Row>
+                {projects ? (
+                  projects.length > 0 ? (
+                    <>
+                      {projects.map((project) => (
+                        <ProjectCard
+                          project={project}
+                          key={project.id}
+                        ></ProjectCard>
+                      ))}
+                    </>
+                  ) : (
+                    <p>No data found in the Projects table.</p>
+                  )
+                ) : (
+                  <p>Loading...</p>
+                )}
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
     </>
   );
 };
