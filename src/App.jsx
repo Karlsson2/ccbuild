f310cc6838cd1d2ae6af312dd088f100fd7c3793
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-09-18 09:20:28 +0200

    created basic header and changed homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Project from "./pages/Project";
 import Product from "./pages/Product";
-
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-        </ul>
-      </nav>
+      <Header></Header>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Projects />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/:projectId" element={<Project />} />
         <Route path="/projects/:projectId/:productId" element={<Product />} />
