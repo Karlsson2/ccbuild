b7d328a0cb4808fb0ee174c3fdad07932b64d1a0
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-09-25 13:32:26 +0200

    fixed project organisation

--- src/components/CreateProject.jsx
+++ src/components/CreateProject.jsx
@@ -1,5 +1,9 @@
-
-import { fetchImage, uploadProjectImage, uploadProductImage, saveImgPathToDb } from "../utils/handleSupabaseImage";
+import {
+  fetchImage,
+  uploadProjectImage,
+  uploadProductImage,
+  saveImgPathToDb,
+} from "../utils/handleSupabaseImage";
 import { supabase } from "../utils/supabase";
 import { useState } from "react";
 
@@ -8,28 +12,28 @@ import "react-datepicker/dist/react-datepicker.css";
 import { Container, Row, Col, Button, Stack } from "react-bootstrap";
 
 export default function CreateProject({ onClose }) {
-  const [name, setName] = useState('');
-  const [country, setCountry] = useState('Sverige');
-  const [region, setRegion] = useState('Göteborg');
+  const [name, setName] = useState("");
+  const [country, setCountry] = useState("Sverige");
+  const [region, setRegion] = useState("Göteborg");
   const [image, setImage] = useState(null);
-  const [currency, setCurrency] = useState('false');
-  const [description, setDescription] = useState('');
-  const [project_number, setProject_Number] = useState('');
-  const [organization, setOrganization] = useState('');
-  const [location_1, setLocation1] = useState('');
-  const [location_2, setLocation2] = useState('');
-  const [location_3, setLocation3] = useState('');
-  const [location_4, setLocation4] = useState('');
-  const [decision_1, setDecision1] = useState('');
-  const [decision_2, setDecision2] = useState('');
-  const [decision_3, setDecision3] = useState('');
-  const [decision_4, setDecision4] = useState('');
-  const [start_date, setStart_date] = useState('');
-  const [end_date, setEnd_date] = useState('');
-  const [first_access_date, setFirst_access_date] = useState('');
-  const [last_access_date, setLast_access_date] = useState('');
-  const [first_delivery_date, setFirst_delivery_date] = useState('');
-  const [last_delivery_date, setLast_delivery_date] = useState('');
+  const [currency, setCurrency] = useState("false");
+  const [description, setDescription] = useState("");
+  const [project_number, setProject_Number] = useState("");
+  const [organization, setOrganization] = useState("Yrgo");
+  const [location_1, setLocation1] = useState("");
+  const [location_2, setLocation2] = useState("");
+  const [location_3, setLocation3] = useState("");
+  const [location_4, setLocation4] = useState("");
+  const [decision_1, setDecision1] = useState("");
+  const [decision_2, setDecision2] = useState("");
+  const [decision_3, setDecision3] = useState("");
+  const [decision_4, setDecision4] = useState("");
+  const [start_date, setStart_date] = useState("");
+  const [end_date, setEnd_date] = useState("");
+  const [first_access_date, setFirst_access_date] = useState("");
+  const [last_access_date, setLast_access_date] = useState("");
+  const [first_delivery_date, setFirst_delivery_date] = useState("");
+  const [last_delivery_date, setLast_delivery_date] = useState("");
 
   const handleImageUpload = async (event) => {
     const file = event.target.files[0];
@@ -42,42 +46,41 @@ export default function CreateProject({ onClose }) {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const { data, error } = await supabase
-      .from('projects')
-      .insert([
-        {
-          name,
-          country,
-          region,
-          image_url: image ? await uploadProjectImage(image) : null,
-          currency,
-          description,
-          project_number,
-          organization,
-          location_1,
-          location_2,
-          location_3,
-          location_4,
-          decision_1,
-          decision_2,
-          decision_3,
-          decision_4,
-          start_date,
-          end_date,
-          first_access_date,
-          last_access_date,
-          first_delivery_date,
-          last_delivery_date,
-        },
-      ]);
+    const { data, error } = await supabase.from("projects").insert([
+      {
+        name,
+        country,
+        region,
+        image_url: image ? await uploadProjectImage(image) : null,
+        currency,
+        description,
+        project_number,
+        organization,
+        location_1,
+        location_2,
+        location_3,
+        location_4,
+        decision_1,
+        decision_2,
+        decision_3,
+        decision_4,
+        start_date,
+        end_date,
+        first_access_date,
+        last_access_date,
+        first_delivery_date,
+        last_delivery_date,
+      },
+    ]);
     if (error) {
-      console.error('Error inserting project: ', error);
+      console.error("Error inserting project: ", error);
     } else {
-        onClose();
-        console.log('Project successfully inserted');
+      onClose();
+      console.log("Project successfully inserted");
     }
   };
 
+  console.log(organization);
   return (
     <div className="full-screen-form d-flex justify-content-center align-items-center">
       <Container>
@@ -105,7 +108,7 @@ export default function CreateProject({ onClose }) {
                     value={country}
                     onChange={(e) => setCountry(e.target.value)}
                   >
-                    <option defaultValue={'Sverige'}>Sverige</option>
+                    <option defaultValue={"Sverige"}>Sverige</option>
                     <option value="Danmark">Danmark</option>
                     <option value="Finland">Finland</option>
                   </select>
@@ -121,7 +124,7 @@ export default function CreateProject({ onClose }) {
                     value={region}
                     onChange={(e) => setRegion(e.target.value)}
                   >
-                    <option defaultValue={'Göteborg'}>Göteborg</option>
+                    <option defaultValue={"Göteborg"}>Göteborg</option>
                     <option value="Gävleborg">Gävleborg</option>
                     <option value="Halland">Halland</option>
                   </select>
@@ -134,7 +137,7 @@ export default function CreateProject({ onClose }) {
                     value={currency}
                     onChange={(e) => setCurrency(e.target.value)}
                   >
-                    <option defaultValue={'SEK'}>SEK</option>
+                    <option defaultValue={"SEK"}>SEK</option>
                     <option value="NOK">NOK</option>
                     <option value="DKK">DKK</option>
                     <option value="EUR">EUR</option>
@@ -146,7 +149,11 @@ export default function CreateProject({ onClose }) {
               <Row className="mb-4">
                 <Col>
                   <label>Projekt-bild</label>
-                  <input type="file" onChange={handleImageUpload} className="form-control" />
+                  <input
+                    type="file"
+                    onChange={handleImageUpload}
+                    className="form-control"
+                  />
                 </Col>
               </Row>
 
@@ -156,7 +163,7 @@ export default function CreateProject({ onClose }) {
                   <textarea
                     className="form-control"
                     placeholder="Beskrivning..."
-                    style={{ height: '100px' }}
+                    style={{ height: "100px" }}
                     id="description"
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}
@@ -172,21 +179,18 @@ export default function CreateProject({ onClose }) {
                     id="project_number"
                     value={project_number}
                     onChange={(e) => setProject_Number(e.target.value)}
-                  >
-                    
-                  </input>
+                  ></input>
                 </Col>
                 <Col>
                   <label>Organisation</label>
                   <select
                     className="form-select mb-3"
                     id="organization"
-                    value={organization}
-                    onChange={(e) => setOrganization(e.target.value)}
+                    value={organization} // Controlled by state
+                    onChange={(e) => setOrganization(e.target.value)} // Updates state
                   >
-                    <option defaultValue={'Yrgo'}>Utbildningskonto - Yrgo</option>
+                    <option value="Yrgo">Utbildningskonto - Yrgo</option>
                     <option value="Annat">annat</option>
-                    
                   </select>
                 </Col>
               </Row>
@@ -297,24 +301,24 @@ export default function CreateProject({ onClose }) {
                   <label>Startdatum</label>
                   <div>
                     <DatePicker
-                        placeholder="Välj datum"
-                        id="start_date"
-                        selected={start_date}
-                        onChange={(date) => setStart_date(date)}
-                        className="form-control"
+                      placeholder="Välj datum"
+                      id="start_date"
+                      selected={start_date}
+                      onChange={(date) => setStart_date(date)}
+                      className="form-control"
                     />
                   </div>
                 </Col>
                 <Col>
-                <div>
-                  <label>Slutdatum</label>
-                  <DatePicker
-                    placeholder="Välj datum"
-                    id="end_date"
-                    selected={end_date}
-                    onChange={(date) => setEnd_date(date)}
-                    className="form-control"
-                  />
+                  <div>
+                    <label>Slutdatum</label>
+                    <DatePicker
+                      placeholder="Välj datum"
+                      id="end_date"
+                      selected={end_date}
+                      onChange={(date) => setEnd_date(date)}
+                      className="form-control"
+                    />
                   </div>
                 </Col>
               </Row>
@@ -323,25 +327,25 @@ export default function CreateProject({ onClose }) {
                 <Col>
                   <label>Första åtkomstdatum</label>
                   <div>
-                  <DatePicker
-                    placeholder="Välj datum"
-                    id="first_access_date"
-                    selected={first_access_date}
-                    onChange={(date) => setFirst_access_date(date)}
-                    className="form-control"
-                  />
+                    <DatePicker
+                      placeholder="Välj datum"
+                      id="first_access_date"
+                      selected={first_access_date}
+                      onChange={(date) => setFirst_access_date(date)}
+                      className="form-control"
+                    />
                   </div>
                 </Col>
                 <Col>
                   <label>Sista åtkomstdatum</label>
                   <div>
-                  <DatePicker
-                    placeholder="Välj datum"
-                    id="last_access_date"
-                    selected={last_access_date}
-                    onChange={(date) => setLast_access_date(date)}
-                    className="form-control"
-                  />
+                    <DatePicker
+                      placeholder="Välj datum"
+                      id="last_access_date"
+                      selected={last_access_date}
+                      onChange={(date) => setLast_access_date(date)}
+                      className="form-control"
+                    />
                   </div>
                 </Col>
               </Row>
@@ -350,35 +354,47 @@ export default function CreateProject({ onClose }) {
                 <Col>
                   <label>Första möjliga leveransdatum</label>
                   <div>
-                  <DatePicker
-                    placeholder="Välj datum"
-                    id="first_delivery_date"
-                    selected={first_delivery_date}
-                    onChange={(date) => setFirst_delivery_date(date)}
-                    className="form-control"
-                  />
+                    <DatePicker
+                      placeholder="Välj datum"
+                      id="first_delivery_date"
+                      selected={first_delivery_date}
+                      onChange={(date) => setFirst_delivery_date(date)}
+                      className="form-control"
+                    />
                   </div>
                 </Col>
                 <Col>
                   <label>Sista möjliga leveransdatum</label>
                   <div>
-                  <DatePicker
-                    placeholder="Välj datum"
-                    id="last_delivery_date"
-                    selected={last_delivery_date}
-                    onChange={(date) => setLast_delivery_date(date)}
-                    className="form-control"
-                  />
+                    <DatePicker
+                      placeholder="Välj datum"
+                      id="last_delivery_date"
+                      selected={last_delivery_date}
+                      onChange={(date) => setLast_delivery_date(date)}
+                      className="form-control"
+                    />
                   </div>
                 </Col>
               </Row>
               <Row>
                 <Stack direction="horizontal" gap={3}>
-                    <Button type="submit" className="btn btn-primary mb-2 ml-2" onClick={handleSubmit}>Submit</Button>
-                
-                    <Button type="button" className="btn btn-secondary mb-2 ml-2" onClick={onClose}>Close</Button>
+                  <Button
+                    type="submit"
+                    className="btn btn-primary mb-2 ml-2"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </Button>
+
+                  <Button
+                    type="button"
+                    className="btn btn-secondary mb-2 ml-2"
+                    onClick={onClose}
+                  >
+                    Close
+                  </Button>
                 </Stack>
-            </Row>
+              </Row>
             </form>
           </Col>
         </Row>
@@ -386,4 +402,3 @@ export default function CreateProject({ onClose }) {
     </div>
   );
 }
-
